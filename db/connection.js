e6bf850ecc312d9f168ddc191f7e10e11abfb0b7
corkesw <home@stephencorke.com>
2021-10-16 09:16:35 +0100

    add log to find error

--- db/connection.js
+++ db/connection.js
@@ -1,27 +1,24 @@
-const { Pool } = require('pg')
-
+const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
-console.log(ENV)
-
-console.log(process.env)
-
 const config = 
-    ENV === 'production'
-        ? {
-            connectionString: process.env.DATABASE_URL,
-            ssl : {
-                rejectUnauthorized: false
-            }
-        }
-        : {}
+  ENV === 'production'
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {};
 
 require('dotenv').config({
-    path: `${__dirname}/../.env.${ENV}`
+  path: `${__dirname}/../.env.${ENV}`,
 });
 
+console.log(process.env.PGDATABASE) /////need to work out why undefined!!!!!
+
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-    throw new Error('PGDATABASE or DATABASE_URL not set');
-  }
-  
-  module.exports = new Pool(config);    
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
+
+module.exports = new Pool(config);
